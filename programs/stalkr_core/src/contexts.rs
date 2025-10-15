0d43fa7c2a4f0959da153af728fa1e7c865e0ed4
stalkr-labs <stalkr-labs@users.noreply.github.com>
2025-10-15 18:54:34 -0500

    implement funding source pattern analysis

---
+++ programs/stalkr_core/src/contexts.rs
@@ -0,0 +1,88 @@
+use anchor_lang::prelude::*;
+use crate::state::*;
+
+#[derive(Accounts)]
+pub struct InitializeTracker<'info> {
+    #[account(
+        init,
+        payer = authority,
+        space = 8 + Tracker::LEN,
+        seeds = [b"tracker"],
+        bump,
+    )]
+    pub tracker: Account<'info, Tracker>,
+    #[account(mut)]
+    pub authority: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(address: Pubkey)]
+pub struct RegisterWhale<'info> {
+    #[account(
+        init,
+        payer = authority,
+        space = 8 + Whale::LEN,
+        seeds = [b"whale", address.as_ref()],
+        bump,
+    )]
+    pub whale: Account<'info, Whale>,
+    #[account(
+        mut,
+        seeds = [b"tracker"],
+        bump = tracker.bump,
+        has_one = authority,
+    )]
+    pub tracker: Account<'info, Tracker>,
+    #[account(mut)]
+    pub authority: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct RecordPrediction<'info> {
+    #[account(
+        init,
+        payer = authority,
+        space = 8 + Prediction::LEN,
+        seeds = [
+            b"prediction",
+            whale.key().as_ref(),
+            &tracker.prediction_count.to_le_bytes(),
+        ],
+        bump,
+    )]
+    pub prediction: Account<'info, Prediction>,
+    #[account(
+        mut,
+        seeds = [b"whale", whale.address.as_ref()],
+        bump = whale.bump,
+    )]
+    pub whale: Account<'info, Whale>,
+    #[account(
+        mut,
+        seeds = [b"tracker"],
+        bump = tracker.bump,
+        has_one = authority,
+    )]
+    pub tracker: Account<'info, Tracker>,
+    #[account(mut)]
+    pub authority: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct ResolvePrediction<'info> {
+    #[account(
+        mut,
+        has_one = authority,
+    )]
+    pub prediction: Account<'info, Prediction>,
+    #[account(
+        mut,
+        seeds = [b"whale", whale.address.as_ref()],
+        bump = whale.bump,
+    )]
+    pub whale: Account<'info, Whale>,
+    pub authority: Signer<'info>,
+}

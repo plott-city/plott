1a1b35231d27d67741e94bad40a8b7fa24f7c488
stalkr-labs <stalkr-labs@users.noreply.github.com>
2025-11-10 07:37:19 -0500

    add prediction feed client (#33)

---
+++ sdk/src/index.ts
@@ -0,0 +1,15 @@
+export { StalkrClient } from "./client";
+export type {
+  WhaleTier,
+  TransactionType,
+  PredictedAction,
+  WhaleProfile,
+  Prediction,
+  PatternAnalysis,
+  FundingSourcePattern,
+  TimeWindowPattern,
+  SequencePattern,
+  TriggerPattern,
+  WatchlistItem,
+  ApiResponse,
+} from "./types";

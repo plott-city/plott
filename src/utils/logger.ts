77e28b1683dd81e2c4f41e96bcd5669986a4f64a
plott-city <216176888+plott-city@users.noreply.github.com>
2025-11-11 12:11:51 +0900

    Update logger to use config module

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,9 +1,11 @@
 import pino from "pino";
+import { config } from "./config";
 
 export const logger = pino({
-  level: process.env.LOG_LEVEL || "info",
+  level: config.LOG_LEVEL,
   transport:
-    process.env.NODE_ENV === "development"
+    config.NODE_ENV === "development"
       ? { target: "pino-pretty", options: { colorize: true } }
       : undefined,
+  base: { service: "plott" },
 });
